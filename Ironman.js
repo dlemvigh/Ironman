e3a0a5af52f98fb9ba4eb2e1387ac5d313db900b
David Lemvigh <dlemvigh@gmail.com>
2015-06-26 17:40:40 +0200

    added entries form and simple list view

--- Ironman.js
+++ Ironman.js
@@ -1,3 +1,5 @@
+Entries = new Mongo.Collection("entries");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -14,6 +16,23 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.entry_form.events({
+    "submit form": function(event){
+      Entries.insert({
+        owner: "me",
+        sport: event.target.sport.value,
+        distance: event.target.distance.value,
+        date: new Date()
+      });
+    }
+  });
+
+  Template.player_entries.helpers({
+    entries: function(){
+      return Entries.find({}, {sort: {date: -1}});
+    }
+  })
 }
 
 if (Meteor.isServer) {
